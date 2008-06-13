180c690a29fab4b65e8f2d3ebd03bcb8e8eed518
mh <mh@immerda.ch>
2008-06-13 20:19:57 +0000

    Merge commit 'puzzle/development'

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,23 @@
-#######################################
-# nfs module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
+#
+# apache module
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 # This module manages an nfs client
 # to manage the nfs server please look
 # into the module nfsd
-#######################################
-
+#
 
 # modules_dir { "nfs": }
+
 class nfs {
     case $operatingsystem {
         centos: { include nfs::centos }
@@ -43,12 +52,7 @@ class nfs::centos inherits nfs::base {
         require => Package[nfs-utils],
     }
 
-    service{rpcgssd:
-        ensure => stopped,
-        enable => false,
-        require => Package[nfs-utils],
-    }
-    service{rpcsvcgssd:
+    service{[ "rpcgssd", "rpcidmapd", "rpcsvcgssd" ]:
         ensure => stopped,
         enable => false,
         require => Package[nfs-utils],
