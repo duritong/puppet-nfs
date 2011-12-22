81a2e93623463657d26f415ec1d1a7db90c89527
mh <mh@immerda.ch>
2011-12-22 19:13:41 +0100

    making it centos 6 ready

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,16 +1,28 @@
 class nfs::centos inherits nfs::base {
-    # note sure if this used on all systems so
-    # moved it to centos
-    service{nfslock:
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package[nfs-utils],
-    }
+# not sure if this used on all systems so
+# moved it to centos
+  service {
+    nfslock :
+      ensure => running,
+      enable => true,
+      hasstatus => true,
+      require => Package[nfs-utils] ;
 
-    service{[ "rpcgssd", "rpcidmapd", "rpcsvcgssd" ]:
-        ensure => stopped,
-        enable => false,
-        require => Package[nfs-utils],
+    ["rpcgssd", "rpcidmapd", "rpcsvcgssd"] :
+      ensure => stopped,
+      enable => false,
+      require => Package[nfs-utils] ;
+  }
+  case $lsbmajdistrelease {
+    '6' : {
+      package {
+        'rpcbind' :
+          ensure => installed,
+      }
+      Service['portmap'] {
+        name => 'rpcbind',
+        require +> Package['rpcbind'],
+      }
     }
+  }
 }
